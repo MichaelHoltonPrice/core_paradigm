139ea85e776fccf698097c8c8d0918e94c62ac6e
sahanasbrmnym <sahana.subramanyam@gmail.com>
2018-08-02 00:41:22 -0600

    directory fixed

--- doc_topic.py
+++ doc_topic.py
@@ -33,9 +33,16 @@ rootDir = os.path.join(homeDir,"core_data")
 outputDir = os.path.join(rootDir,"pickles")
 
 model =  models.LdaModel.load(os.path.join(rootDir,'lda_output/ldamodel_output100'))
-"""
-#getting textbooks
 
+#model_old = models.LdaModel.load('/home/sahana/lda_save/ldamodel_output100')
+#model_partial = models.LdaModel.load('/home/sahana/sfi_research/lda_output/ldamodel_output100')
+#getting textbooks
+#path_txt = "/home/sahana/sfi_research/pickles/textbooks.pkl"
+
+#with open(path_txt,"rb") as f:
+#    textbooks = pickle.load(f)
+    
+#this step is necessary. DO RUN!
 with open(os.path.join(outputDir,"textbooks.pkl"),"rb") as f:
     textbooks = pickle.load(f)
 
@@ -47,9 +54,23 @@ tokenizer = RegexpTokenizer(r'\w+')
 stemmer = PorterStemmer()
 cleaned_texts = []
 
-#bigramming
 
-sentence_split = [i.split(" ") for i in textbooks]
+#stem toke stop
+for i in range(len(textbooks)):
+    #tokenize document
+    #lower_case = training_flat[i].lower()
+    tokens = tokenizer.tokenize(textbooks[i])
+    
+    #stopwords
+    stopped = [i for i in tokens if not i in stopwords]
+    
+    #stemmed
+    stemmed = [stemmer.stem(i) for i in stopped]
+    
+    cleaned_texts.append(stemmed)
+    
+sent_text = [" ".join(i) for i in cleaned_texts]
+sentence_split = [i.split(" ") for i in sent_text]
 
 phrases = Phrases(sentence_split)
 
@@ -60,34 +81,25 @@ bigrammed = list(bigrammed[sentence_split])
 
 bigrammed_textbooks = [' '.join(i) for i in bigrammed if len(i) > 1]
 
-#stem toke stop
-for i in range(len(bigrammed_textbooks)):
-    #tokenize document
-    #lower_case = training_flat[i].lower()
-    tokens = tokenizer.tokenize(bigrammed_textbooks[i])
-    
-    #stopwords
-    stopped = [i for i in tokens if not i in stopwords]
-    
-    #stemmed
-    stemmed = [stemmer.stem(i) for i in stopped]
-    
-    cleaned_texts.append(stemmed)
-
-doc_corpus = cleaned_texts
+doc_corpus = bigrammed
 
 with open(os.path.join(outputDir,"doc_corpus.pkl"),"wb") as f:
     pickle.dump(doc_corpus,f)
 
-"""
+
+with open("/home/sahana/sfi_research/pickles/doc_corpus.pkl","wb") as f:
+    pickle.dump(doc_corpus,f)
+
+
 with open(os.path.join(outputDir,"doc_corpus.pkl"),"rb") as f:
     doc_corpus = pickle.load(f)
 
+
 doc_corpus_codes = ["samuelson","marx","hayek","krugman","sam_nord","acemoglu","smith","keynes","CORE","marshall","mill","mankiw"]
 #tokenizing
-tokenizer = RegexpTokenizer(r'\w+')
+#tokenizer = RegexpTokenizer(r'\w+')
 
-#doc_corpus = [tokenizer.tokenize(i) for i in doc_corpus]
+#doc_corpus = [tokenizer.tokenize(i) for i in textbooks]
 
 txtbook_dictionary = corpora.Dictionary(doc_corpus)
 corpus = [txtbook_dictionary.doc2bow(t)for t in doc_corpus]
